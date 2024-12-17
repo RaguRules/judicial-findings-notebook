0e6b47ef25a6fca43af487cf69c8683de7830552
Ragu <sriraguraj@gmail.com>
2024-12-17 22:48:09 +0530

    upd

--- api/V2/checkdb.php
+++ api/V2/checkdb.php
@@ -1,7 +1,13 @@
 <?php
+echo "hi";
 
-require_once "../../lib/db.class.db";
+require_once "../../lib/db.class.php";
 
-public function Conn(){
+function conn(){
+    echo "API starts...";
+    $db = new Database();
+    $conn = $db->getConnection();
 
 }
+
+$conn = conn();
